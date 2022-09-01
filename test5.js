1b21edec22b9e5fcbbeca6030146e758a9383500
Avinash Kumar <avinashkumar@Admins-MacBook-Air.local>
2022-09-01 16:02:38 +0530

    pehla commit

---
+++ test5.js
@@ -0,0 +1,27 @@
+const posts= [
+    { title: 'post 1', body : 'this is title 1'},
+{ title: 'post 2', body : 'this is title 1'}
+];
+
+function getpost(){
+    setTimeout(()=>{
+        let output = '';
+        posts.forEach((post,index)=>{
+            output+= `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output
+    }, 1000)
+}
+
+
+function createPost(post, callback){
+    setTimeout(()=> {
+        posts.push(post);
+        callback()
+    
+    },2000)
+}
+
+
+
+createPost({title:'post 3', body: 'this is title 3'},getpost);
